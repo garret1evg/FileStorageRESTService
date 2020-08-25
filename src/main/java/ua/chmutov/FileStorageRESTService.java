09fcec38ec1a3ec0d8166f3ff93b19497f80078f
Evgenii Chmutov <garret.evg@gmail.com>
2020-08-25 13:35:55 +0300

    configured spring app

---
+++ src/main/java/ua/chmutov/FileStorageRESTService.java
@@ -0,0 +1,11 @@
+package ua.chmutov;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FileStorageRESTService {
+    public static void main(String[] args) {
+        SpringApplication.run(FileStorageRESTService.class, args);
+    }
+}
